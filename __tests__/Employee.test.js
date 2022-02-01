943ab3496a22497bd2af53894ad55e83ad841501
Mike Gordon <vprmatrix55@gmail.com>
2022-01-31 19:21:52 -0700

    added employee tests and functions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,34 @@
 const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Mike', 55, 'myemail@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+
+});
+
+test('gets the employee name', () => {
+    const employee = new Employee('Mike', 55, 'myemail@gmail.com');
+
+    expect(employee.getName()).toEqual('Mike');
+});
+
+test('gets the employee id', () => {
+    const employee = new Employee('Mike', 55, 'myemail@gmail.com');
+
+    expect(employee.getId()).toEqual(55);
+});
+
+test('gets the employee email', () => {
+    const employee = new Employee('Mike', 55, 'myemail@gmail.com');
+
+    expect(employee.getEmail()).toEqual('myemail@gmail.com');
+});
+
+test('gets the employee role', () => {
+    const employee = new Employee('Mike', 55, 'myemail@gmail.com');
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+})
