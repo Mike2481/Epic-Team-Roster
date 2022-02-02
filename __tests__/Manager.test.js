d8ab85414cb07226ef2c89fb77e33d3dc341b0f6
Mike Gordon <vprmatrix55@gmail.com>
2022-02-01 19:08:22 -0700

    add manager tests and functions

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,19 @@
+const Manager = require('../lib/Manager');
+
+test('creates manager object', () => {
+    const manager = new Manager('Mike', 55, 'myemail@gmail.com', 223);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('gets office number', () => {
+    const manager = new Manager('Mike', 55, 'myemail@gmail.com', 223);
+
+    expect(manager.getOfficeNumber()).toEqual(223);
+});
+
+test('gets employee role', () => {
+    const manager = new Manager('Mike', 55, 'myemail@gmail.com', 223);
+
+    expect(manager.getRole()).toEqual('Manager');
+});
